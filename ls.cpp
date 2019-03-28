d3c9bb8418d35ebba2ee82d0d8e77af748aff3a9
David Wartenbe <thedavinatorw@gmail.com>
2019-03-27 20:43:32 -0500

    fixes

--- ls.cpp
+++ ls.cpp
@@ -7,36 +7,6 @@
 
 using namespace std;
 
-/*function... might want it in some class?*/
-// int getdir (string dir, vector<string> &files)
-// {
-//     DIR *dp;
-//     struct dirent *dirp;
-//     if((dp = opendir(dir.c_str())) == NULL) {
-//         cout << "Error(" << errno << ") opening " << dir << endl;
-//         return errno;
-//     }
-
-//     while ((dirp = readdir(dp)) != NULL) {
-//         files.push_back(string(dirp->d_name));
-//     }
-//     closedir(dp);
-//     return 0;
-// }
-
-// int main()
-// {
-
-//     string dir = string(".");
-//     vector<string> files = vector<string>();
-
-//     getdir(dir,files);
-
-//     for (unsigned int i = 0;i < files.size();i++) {
-//         cout << files[i] << endl;
-//     }
-//     return 0;
-// }
 int getdir(string dir, vector<string> &files)
 {
     DIR *directory;
